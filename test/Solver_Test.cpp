59b1f9963b66b032e5fc4cd0070e463a5f9f7255
Dylan O <douell8@uwo.ca>
2025-10-29 20:08:31 -0400

    add flto compiler option

--- test/Solver_Test.cpp
+++ test/Solver_Test.cpp
@@ -71,7 +71,7 @@ TEST_F(SoverTest, DoubleLetterTest) { // 0.8s
 }
 
 
-TEST_F(SoverTest, DifficultWords) { // 3.85484 in 53.5s
+TEST_F(SoverTest, DifficultWords) { // 3.85484 in 39.8s
   std::fstream file("../data/difficult_wordlist.txt");
 
   ASSERT_TRUE(file.is_open());
